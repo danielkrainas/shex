2f10f933650d8bacea5cf562ce743c8a9c1ae2d9
Danny Krainas <dkrainas87@gmail.com>
2017-03-11 14:19:46 -0500

    fill in api cmd

--- cmd/api/command.go
+++ cmd/api/command.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/danielkrainas/gobag/cmd"
 	"github.com/danielkrainas/gobag/context"
+
+	"github.com/danielkrainas/shex/api/server"
+	"github.com/danielkrainas/shex/registry/configuration"
 )
 
 func init() {
@@ -13,15 +16,24 @@ func init() {
 }
 
 func run(ctx context.Context, args []string) error {
+	config, err := configuration.Resolve(args)
+	if err != nil {
+		return err
+	}
 
-	return nil
+	s, err := server.New(ctx, config)
+	if err != nil {
+		return err
+	}
+
+	return s.ListenAndServe()
 }
 
 var (
 	Info = &cmd.Info{
 		Use:   "api",
 		Short: "run the api server",
-		Long:  "run the api server",
+		Long:  "Run the api server.",
 		Run:   cmd.ExecutorFunc(run),
 	}
 )
