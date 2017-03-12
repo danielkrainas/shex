e33776a512442f3099978a959575460b6ae84a54
Danny Krainas <dkrainas87@gmail.com>
2017-03-12 18:00:14 -0400

    fix error where mods endpoint returned null

--- registry/storage/driver/inmemory/inmemory.go
+++ registry/storage/driver/inmemory/inmemory.go
@@ -3,6 +3,7 @@ package inmemory
 import (
 	"github.com/danielkrainas/gobag/decouple/drivers"
 
+	"github.com/danielkrainas/shex/api/v1"
 	"github.com/danielkrainas/shex/registry/storage"
 	"github.com/danielkrainas/shex/registry/storage/driver/factory"
 )
@@ -26,7 +27,10 @@ type driver struct {
 func (d *driver) Mods() storage.ModStore {
 	store, ok := d.stores[" mod"].(storage.ModStore)
 	if !ok {
-		store = &modStore{}
+		store = &modStore{
+			mods: make([]*v1.ModInfo, 0),
+		}
+
 		d.stores[" mod"] = store
 	}
 
